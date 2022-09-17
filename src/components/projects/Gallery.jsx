95061e1e3391bcdd9e45acf152c64a5955f0aaa3
MD Ripon Islam <islam876ripon@gmail.com>
2022-09-17 17:31:51 +0600

    sample img remove

--- src/components/projects/Gallery.jsx
+++ src/components/projects/Gallery.jsx
@@ -11,7 +11,6 @@ function Gallery() {
 	const [filterKey, setFilterKey] = useState('*');
 	const [items, setItems] = useState([]);
     const parent = useRef(null);
-    const imgUrl  = "https://img.freepik.com/free-vector/luxury-white-ramadan-greeting-card-islamic-background-with-eid-mubarak-illustration_511846-189.jpg";
   
 useEffect(() => {
 	setItems(parent.current.children)
@@ -51,18 +50,9 @@ useEffect(() => {
 	
   
 			<div className="row p-5 justify-content-center itemsContainer" ref={parent}>
-			{projects?.map((project)=>
-				<ProjectItem img={project.img} labels={project.category} title={project.title} link={project.link} />
-			)}
-				
-				{/*<ProjectItem img={imgUrl} labels="react"/>
-				<ProjectItem img={imgUrl} labels="full_stack"/>
-				<ProjectItem img={imgUrl} labels="full_stack"/>
-			 	<ProjectItem img={imgUrl} labels="html_&_css"/>
-				<ProjectItem img={imgUrl} labels="react"/>
-				<ProjectItem img={imgUrl} labels="html_&_css"/>
-				<ProjectItem img={imgUrl} labels="full_stack"/>
-				<ProjectItem img={imgUrl} labels="package"/>*/}
+				{projects?.map((project)=>
+					<ProjectItem img={project.img} labels={project.category} title={project.title} link={project.link} />
+				)}
 			</div>
        
 		</div>
